0cca5aa2cd3391cbadd90903983cec874ba4d071
Naomi <8650521+de-ocampo@users.noreply.github.com>
2020-03-29 01:37:20 -0700

    Add firebase, start setting up redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+if (root !== null) {
+  ReactDOM.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App/>
+        </Provider>
+      </React.StrictMode>,
+      root,
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,1 +1,11 @@
 // @flow
+import {FETCH_ENTRIES} from '../actions';
+
+export default (state: Object = {}, action: Object) => {
+  switch (action.type) {
+    case FETCH_ENTRIES:
+      return action.payload;
+    default:
+      return state;
+  }
+};
